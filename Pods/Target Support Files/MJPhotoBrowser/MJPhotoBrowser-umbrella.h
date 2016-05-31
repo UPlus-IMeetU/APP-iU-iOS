4f0afe63fdf88ce0fbabc4641cbf3833dac5d22f
TheWindRises <spring@springdemacbook-pro.local>
2016-05-31 19:56:20 +0800

    追加MJBrowser

---
+++ Pods/Target Support Files/MJPhotoBrowser/MJPhotoBrowser-umbrella.h
@@ -0,0 +1,12 @@
+#import <UIKit/UIKit.h>
+
+#import "MJPhoto.h"
+#import "MJPhotoBrowser.h"
+#import "MJPhotoLoadingView.h"
+#import "MJPhotoProgressView.h"
+#import "MJPhotoToolbar.h"
+#import "MJPhotoView.h"
+
+FOUNDATION_EXPORT double MJPhotoBrowserVersionNumber;
+FOUNDATION_EXPORT const unsigned char MJPhotoBrowserVersionString[];
+

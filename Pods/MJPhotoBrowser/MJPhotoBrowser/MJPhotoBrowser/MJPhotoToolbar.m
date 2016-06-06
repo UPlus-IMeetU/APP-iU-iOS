fb3d3cfddc4d7ece7527817d6203a79582922891
TheWindRises <spring@springdemacbook-pro.local>
2016-06-06 16:47:28 +0800

    进行BUG修改

--- Pods/MJPhotoBrowser/MJPhotoBrowser/MJPhotoBrowser/MJPhotoToolbar.m
+++ Pods/MJPhotoBrowser/MJPhotoBrowser/MJPhotoBrowser/MJPhotoToolbar.m
@@ -48,8 +48,8 @@
     _saveImageBtn = [UIButton buttonWithType:UIButtonTypeCustom];
     _saveImageBtn.frame = CGRectMake(20, 0, btnWidth, btnWidth);
     _saveImageBtn.autoresizingMask = UIViewAutoresizingFlexibleHeight;
-    [_saveImageBtn setImage:[UIImage imageNamed:@"MJPhotoBrowser.bundle/save_icon.png"] forState:UIControlStateNormal];
-    [_saveImageBtn setImage:[UIImage imageNamed:@"MJPhotoBrowser.bundle/save_icon_highlighted.png"] forState:UIControlStateHighlighted];
+    [_saveImageBtn setImage:[UIImage imageNamed:@"save_icon.png"] forState:UIControlStateNormal];
+    [_saveImageBtn setImage:[UIImage imageNamed:@"save_icon_highlighted.png"] forState:UIControlStateHighlighted];
     [_saveImageBtn addTarget:self action:@selector(saveImage) forControlEvents:UIControlEventTouchUpInside];
     [self addSubview:_saveImageBtn];
 }
